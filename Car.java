7aa8e64c5566513759244bd74aa71da39fd2caf7
muradii <39276226+muradii@users.noreply.github.com>
2018-06-17 21:05:33 -0600

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,29 @@
+package extra.pkgclass.car.race;
+import java.util.Random;
+
+
+public class Car {
+    String brand;
+    int year;
+    int horsePower;
+    int distance;
+    int i;
+    
+    Car(String _brand, int _year, int _horsePower){
+        brand = _brand;
+        year = _year;
+        horsePower = _horsePower;
+    }
+    
+    public int advance (){
+        while (distance<1000){
+            Random Rand = new Random();
+            int random = Rand.nextInt(2);
+            distance = (horsePower*random) + distance;  
+            i++;
+        }
+        return distance;
+        
+    }
+}
+
